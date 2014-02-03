a283c20637ab03ca85b74c7af9c019b4130ff331
Jason Normandin <jason.n@nicheinfo.net>
2014-02-03 10:37:39 -0800

    testing

--- mu-plugins/wp-pluginizer-libs/libs/WPPlgnzrDebugLog.php
+++ mu-plugins/wp-pluginizer-libs/libs/WPPlgnzrDebugLog.php
@@ -7,9 +7,6 @@
  */
 class WPPlgnzrDebugLog {
   public $lg = false;
-  public function __construct(){
-    die('testing for new commit');
-  }
   public function start($filename=false){
     $filename = ($filename)?$filename:ABSPATH.'/wp_content/uploads/debug_log_'.date('Y-m-d').'.txt';
     $this->lg = @fopen($filename,'a+');
